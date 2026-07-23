680013d1806bccc84daf2f114939e895782a83d0
BarryNorfolk <barrynorfolkman@protonmail.com>
2026-07-23 18:52:50 +0200

    Add new node network for blobs

---
+++ Content.Server/_DV/Blob/BlobAntagNet.cs
@@ -0,0 +1,9 @@
+using Content.Server.NodeContainer.NodeGroups;
+using Content.Shared.NodeContainer.NodeGroups;
+
+namespace Content.Server._DV.Blob;
+
+[NodeGroup(NodeGroupID.BlobAntag)]
+public sealed class BlobAntagNet : BaseNodeGroup
+{
+}
---
+++ Content.Server/_DV/Blob/BlobAntagNode.cs
@@ -0,0 +1,27 @@
+using Content.Server.NodeContainer.Nodes;
+using Content.Shared.NodeContainer;
+using Robust.Shared.Map.Components;
+
+namespace Content.Server._DV.Blob;
+
+[DataDefinition]
+public sealed partial class BlobAntagNode : Node
+{
+    public override IEnumerable<Node> GetReachableNodes(TransformComponent xform,
+        EntityQuery<NodeContainerComponent> nodeQuery,
+        EntityQuery<TransformComponent> xformQuery,
+        MapGridComponent? grid,
+        IEntityManager entMan)
+    {
+        if (grid == null)
+            yield break;
+
+        var gridIndex = grid.TileIndicesFor(xform.Coordinates);
+
+        foreach (var (_, node) in NodeHelpers.GetCardinalNeighborNodes(nodeQuery, grid, gridIndex))
+        {
+            if (node != this)
+                yield return node;
+        }
+    }
+}
